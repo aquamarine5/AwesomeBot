80dd07b1cc251151a5927128920e2c0715c71fa9
awesomehhhhh <50990424+awesomehhhhh@users.noreply.github.com>
2020-08-11 18:54:07 +0800

    add help

--- python/webyxp.py
+++ python/webyxp.py
@@ -8,6 +8,14 @@ if(is_pydroid):
     arg="python yxpRs 1585738 最新".split(" ")
 else:
     arg=sys.argv
+help='''python webyxp.py yxpRs 1585745 最新 -*看这个id的最新已批改成绩和班级平均分比较
+python webyxp.py yxpAs 1585745 语文1 -*看这个id下语文的第1个没写作业的答案
+python webyxp.py yxpPic 1585745 -*看这个id的私人头像
+python webyxp.py yxpInfo 1585745 -*暂未实现
+python webyxp.py yxpHw 1585745 没写 -*所有没写的作业
+python webyxp.py yxpHw 1585745 语文 -*语文没写的作业
+脚本需要在cmd下运行，或者把 is_pydroid设置 true然后在arg下写命令。
+'''
 def yxpTimeGet():
     urlT="http://e.anoah.com/api_dist/?q=json/ebag/System/getServerTime&info={}"
     out=requests.get(urlT)

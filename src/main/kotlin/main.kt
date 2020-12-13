eee42492ca1df39f38b85601aaa8bc0e4ae13f3d
awesomehhhhh <50990424+awesomehhhhh@users.noreply.github.com>
2020-12-13 21:22:05 +0800

    fix many little problem and add a little thing

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -20,7 +20,6 @@ const val func="\"d:/Program Source/QQBOT/python/func.py\""
 const val image="\"d:/Program Source/QQBOT/python/image.py\""
 const val temp="D:/Program Source/QQBOT/python/Temp/temp.txt"
 const val elog="D:/Program Source/QQBOT/python/Temp/t.log"
-const val tempCheck="D:/Program Source/QQBOT/python/Temp/check.txt"
 const val imageTemp="D:/Program Source/QQBOT/python/Temp/temp.jpg"
 const val imageMath="D:/Program Source/QQBOT/python/Temp/Math.png"
 const val imagePrivate="D:/Program Source/QQBOT/python/Temp/FacePrivate.jpg"
@@ -34,7 +33,7 @@ suspend fun main() {
         fileBasedDeviceInfo("device.json")
         protocol=BotConfiguration.MiraiProtocol.ANDROID_PHONE
     }.alsoLogin()
-    var type=1
+    var type: Int
     var photopath=""
     var command=""
     miraiBot.getGroup(1074494974L).sendMessage("qq bot start on all group,and will recall in 10 second.").recallIn(30000)
@@ -54,7 +53,7 @@ suspend fun main() {
         print(event.message[Image]?.queryUrl()+"\n")
         try{
            val message = event.message.content
-           check(event)
+           if(!check(event))return@subscribeAlways
            if(message=="机器人 off"){
                 reply(At(event.sender as Member)+" 呜呜呜有人要关我")
                 return@subscribeAlways
@@ -69,9 +68,7 @@ suspend fun main() {
                 "yxpAs", "yxp答案" -> command = "python $program yxpAs ${ct[1]} ${ct[2]}${ct[3]}"
                 "yxpPic", "yxppic", "yxp照片" -> {
                     type=0
-                    command = "python $program yxpPic ${ct[1]}"
-                    val rt=command.execute()
-                    rt.waitFor()
+                    "python $program yxpPic ${ct[1]}".execute().waitFor()
                     val file = File(temp)
                     if (file.readText() == "") {
                         reply("用户不存在")
@@ -107,19 +104,17 @@ suspend fun main() {
                 }
                 "数学题", "math" -> {
                     type=2
-
                     command = "python $webapi math"
                     photopath=imageMath
                 }
                 "zyb","作业帮","作业"-> {
                     type = 0
-                    var num = -1
-                    num = if (ct.size == 2) {
+                    val num: Int = if (ct.size == 2) {
                         0
                     } else {
                         ct[2].toInt()
                     }
-                    val msg = "python $webapi zyb ${ct[1]} $num".execute().waitForThis().inputStream.readString().split("{img}")
+                    val msg = "python $webapi zyb ${ct[1]} $num".runExecute().split("{img}")
                     buildMessageChain {
                         if (msg.size == 1) {
                             add(msg[0])
@@ -135,8 +130,7 @@ suspend fun main() {
                     }.send()
                 }
                 "trsWd", "翻译单词", "trswd" -> {
-                    var engine = ""
-                    engine = if (ct.size == 2) {
+                    val engine: String = if (ct.size == 2) {
                         "-"
                     } else {
                         ct[2]
@@ -168,7 +162,7 @@ suspend fun main() {
 
                 "搜索建议", "idea", "search" -> {
                     type=0
-                    reply("python $webapi search ${ct[1]}".execute().waitForThis().inputStream.readString()).recallIn(60000)
+                    reply("python $webapi search ${ct[1]}".runExecute()).recallIn(60000)
                 }
                 "baidu","百度"->command = "python $webapi baidu ${ct[1]}"
                 "hotword","热词"->command = "python $webapi hotword"
@@ -176,7 +170,7 @@ suspend fun main() {
                 "news","新闻"->command = "python $webapi news ${ct[1]}"
                 "face","头像"->{
                     type=0
-                    val result="python $webapi face b".execute().waitForThis().inputStream.readString()
+                    val result="python $webapi face b".runExecute()
                     print("D:\\Program Source\\QQBOT\\python\\Temp\\Face\\${result.trim().toInt()}.jpg\n")
                     try{
                         File("D:\\Program Source\\QQBOT\\python\\Temp\\Face\\${result.trim().toInt()}.jpg").sendAsImage()
@@ -188,14 +182,14 @@ suspend fun main() {
                 "help", "帮助" -> {
                     type=0
                     if(ct.size == 1){
-                    reply("""->括号内为简写如："搜索建议 机器人"可替换成"idea 机器人"<-
+                        reply("""->括号内为简写如："搜索建议 机器人"可替换成"idea 机器人"<-
 github.com/awesomehhhhh/AwesomeBot
 翻译 需要翻译的文字 翻译的语种 -> 返回翻译结果（trs）
 头像 ->返回某个b站头像（face）
 热词 ->返回b站热词（hotword）
 热搜 ->返回微博热搜（hot）
-百度 内容 ->返回百度搜索的内容（测试中）（百度）
-翻译单词 需要翻译的单词 -> 返回单词英文结果和相关信息（trsWd）
+百度 内容 ->返回百度搜索的内容（测试中）（百度）""".trimIndent()).recallIn(30000)
+                        reply("""翻译单词 需要翻译的单词 -> 返回单词英文结果和相关信息（trsWd）
 搜索建议 信息 ->返回搜索建议（idea）
 帮助 ->返回帮助（help）
 作业帮 题目 ->返回作业帮搜题（zyb）
@@ -203,48 +197,46 @@ github.com/awesomehhhhh/AwesomeBot
 性别判断 人名字 ->根据人名判断性别（ng）
 eat 文字 ->生成表情包
 二维码生成 内容 ->生成二维码（qrcode）（仅限英文）
-数学题 ->返回数学题""".trimIndent())}
+数学题 ->返回数学题""".trimIndent()).recallIn(31000)
+                        reply("""更多帮助请发送：
+help 翻译
+help 图片
+优学派帮助""".trimMargin()).recallIn(32000)
+                    }
                     else if (ct.size == 2){
-                        if(ct[1]=="谷歌翻译"){
-                            reply("""支持的翻译语种：
+                        when(ct[1]) {
+                            "谷歌翻译" -> reply("""支持的翻译语种：
 （另：谷歌翻译服务器日常土豆）
 简体中文，繁体中文，文言文，粤语，
 日语，英语，德语，法语，韩语，芬兰语，犹太语，祖鲁语，南非语，希腊语，
 孟加拉语，加泰罗尼亚语，保加利亚语，库尔德语，葡萄牙语，阿拉伯语
-象形（阿姆哈拉文）
-""".trimIndent())
-                        }
-                        else if(ct[1]=="百度翻译"){
-                        reply("""
+象形（阿姆哈拉文）""".trimIndent()).recallIn(30000)
+
+                            "百度翻译" -> reply("""
 #支持的语种（即翻译关键字）：
  中文->粤语，粤语->中文（翻译关键字：粤语中文），文言文->中文（翻译关键字：文言文中文），中文->文言文
  韩语，葡萄牙语，阿拉伯语，荷兰语，英语，日语，德语，保加利亚语，希腊语
 # 用法：
- 翻译 [翻译关键字] [需要翻译的内容]
-                        """.trimIndent())}
-                        else if(ct[1]=="图片"){
-                            reply("""
-# 用法： 图片 [参数]
+ 翻译 [翻译关键字] [需要翻译的内容]""".trimIndent()).recallIn(30000)
+                            "图片" -> reply("""# 用法： 图片 [参数]
 目前参数只能为数字（1-9）
 不传参则随机
 3是宠物，5是明星
-其他暂时未知
-                            """.trimIndent())
-                        }
-                        else if(ct[1]=="翻译"){
-                            reply("请输入\"help 百度翻译\"或\"help 谷歌翻译\"")
+其他暂时未知""".trimIndent()).recallIn(30000)
+                            "翻译" -> reply("请输入\"help 百度翻译\"或\"help 谷歌翻译\"")
                         }
                     }
                 }
-                "yxpHelp", "yxp帮助" -> {
+                "yxpHelp", "yxp帮助" ,"优学派帮助" -> {
                     reply("""
 --优学派方面（快 没 用 了）--
+github.com/awesomehhhhh/EbagUtil
 yxp照片 uid ->返回uid对应的照片
 yxp作业 uid 没写 ->返回uid所有没写作业
 yxp答案 uid 学科 第几个 ->返回uid指定学科的没写作业的答案
 yxp信息 uid ->返回uid对应信息
 yxp积分 uid ->返回uid对应积分
-yxp老师评语 uid ->返回uid作业评语""".trimIndent())
+yxp老师评语 uid ->返回uid作业评语""".trimIndent()).recallIn(30000)
                 }
                 "ip" -> command = "python $webapi ip ${ct[1]}"
                 "pos" -> command = "python $webapi pos ${ct[1]}"
@@ -257,20 +249,22 @@ yxp老师评语 uid ->返回uid作业评语""".trimIndent())
                 }
                 "photo","图片","每日一图"->{
                     type=0
-                    val index:Int = if(ct.size==1) -1;
-                    else if(ct[1].length>=2){
-                        reply("数字过大，仅支持1-9（包括1-9）")
-                        print(3)
-                        return@subscribeAlways
-                    } else {
-                        try{
-                            ct[1].toInt()
-                        }catch (e:Exception){
-                            reply("参数不是数字")
+                    val index:Int = when {
+                        ct.size==1 -> -1
+                        ct[1].length>=2 -> {
+                            reply("数字过大，仅支持1-9（包括1-9）")
                             return@subscribeAlways
                         }
+                        else -> {
+                            try{
+                                ct[1].toInt()
+                            }catch (e:NumberFormatException){
+                                reply("参数不是数字")
+                                return@subscribeAlways
+                            }
+                        }
                     }
-                    val f="python $webapi photo $index".execute().waitForThis().inputStream.readString().split("|")
+                    val f="python $webapi photo $index".runExecute().split("|")
                     try{
                         reply(f[1].replace("_", "\n"))
                         File(f[0]).sendAsImage()
@@ -282,7 +276,7 @@ yxp老师评语 uid ->返回uid作业评语""".trimIndent())
                 "性别判断", "ng", "Sex" -> command = "python $func ng ${ct[1]}"
                 "hot","微博热搜","wb","微博" -> {
                     type=0
-                    reply("python $webapi hot".execute().waitForThis().inputStream.readString()).recallIn(30000)
+                    reply("python $webapi hot".runExecute()).recallIn(30000)
                 }
                 else->{
                     type=100
@@ -290,7 +284,7 @@ yxp老师评语 uid ->返回uid作业评语""".trimIndent())
             }
             when (type){
                 1->{
-                    reply(command.execute().waitForThis().inputStream.readString())
+                    reply(command.runExecute())
                     type=0
                 }
                 2->{
@@ -309,22 +303,27 @@ yxp老师评语 uid ->返回uid作业评语""".trimIndent())
     miraiBot.join() // 等待 机器人 离线, 避免主线程退出
 }
 
-suspend fun check(event: MessageEvent){
-    if(event.message.content.length<50)return
-    if(event.message.content.contains("<?xml version='1.0' encoding='UTF-8' standalone='yes'?>"))return
-    if(event.sender.id== 2854196310L)return
-    if((event.sender as Member).permission.level==1)return
-    if(event.message.content.contains("bilibili.com"))return
-    if(event.message.content.contains("docs.microsoft.com"))return
-    if(event.message.content.contains("unity.cn"))return
-    if(event.message.content.contains("unity.com"))return
-    if(event.message.content.contains("csdn.com"))return
-    if(event.message.content.contains("github.com"))return
-    val result="python $webapi check \"${event.message.content}\"".execute().waitForThis().inputStream.readString()
-    println(result)
+suspend fun check(event: MessageEvent):Boolean{
+    val message=event.message.content
+    if(message.length<50)return true
+    if(message.contains("<?xml version='1.0' encoding='UTF-8' standalone='yes'?>"))return true
+    if(event.sender.id== 2854196310L)return true
+    if((event.sender as Member).permission.level==1)return true
+    if(message.contains("bilibili.com"))return true
+    if(message.contains("docs.microsoft.com"))return true
+    if(message.contains("unity.cn"))return true
+    if(message.contains("unity.com"))return true
+    if(message.contains("csdn.com"))return true
+    if(message.contains("github.com"))return true
+    val result="python $webapi check \"${event.message.content}\"".runExecute()
     if(event.message.content.contains("感兴趣的可以加一下进去交流学习哦")) checkFailed(event,result)
-    if (result.contains("通过")) print(result)
-    else checkFailed(event,result)
+    return if (result.contains("通过")) {
+        print(result)
+        true
+    } else {
+        checkFailed(event,result)
+        false
+    }
 }
 
 suspend fun checkFailed(event: MessageEvent,result:String){
@@ -345,6 +344,7 @@ fun Process.waitForThis():Process{
     this.waitFor()
     return this
 }
+fun String.runExecute() : String { return this.execute().waitForThis().inputStream.readString() }
 fun InputStream.readString():String{
     val bos=ByteArrayOutputStream()
     try{
